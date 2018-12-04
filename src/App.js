42d04b2a4fe582cc9ed17aa5ad8ffbae740cd1e4
Alina <alex.alina.rusu@gmail.com>
2018-12-04 12:03:40 +0100

    setup react app and connected it with redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
+import EventsListContainer from './components/EventsListContainer';
+// import LoginPage from './components/login/LoginPage'
+// import SignupPage from './components/signup/SignupPage'
+// import LogoutPage from './components/logout/LogoutPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+<Router>
+        <div>
+          {/* <nav>
+            <TopBar />
+          </nav> */}
+          <main style={{marginTop:75}}>
+            {/* <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/logout" component={LogoutPage} />
+            <Route exact path="/signup" component={SignupPage} /> */}
+            <Route exact path="/events" component={EventsListContainer} />
+            <Route exact path="/" render={ () => <Redirect to="/events" /> } />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/EventsListContainer.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function EventsListContainer(props) {
+  return (<h1>Welcome to my events page!</h1>)
+}
